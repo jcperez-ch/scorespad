4792722661fd45457f2b8a39740ce39564e050c5
JC Perez Chavez <jc.perezchavez@gmail.com>
2018-11-20 11:23:54 -0500

    Updates

---
+++ precache-manifest.d03942ed6177c060f118053be792ab62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4324ed7f4482edc3c9f4",
+    "url": "/scorespad/static/js/runtime~main.4324ed7f.js"
+  },
+  {
+    "revision": "d2dae9195bf1e225a200",
+    "url": "/scorespad/static/js/main.d2dae919.chunk.js"
+  },
+  {
+    "revision": "24afd148890b46f910e7",
+    "url": "/scorespad/static/js/1.24afd148.chunk.js"
+  },
+  {
+    "revision": "d2dae9195bf1e225a200",
+    "url": "/scorespad/static/css/main.15331104.chunk.css"
+  },
+  {
+    "revision": "24afd148890b46f910e7",
+    "url": "/scorespad/static/css/1.019e697e.chunk.css"
+  },
+  {
+    "revision": "228f6958e48ee7374d04f8001dbe6e9d",
+    "url": "/scorespad/index.html"
+  }
+];
