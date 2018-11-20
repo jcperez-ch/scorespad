06b1c0f0ea6b2d78a0f0fe4b03ce942cb4414a6a
JC Perez Chavez <jc.perezchavez@gmail.com>
2018-11-20 11:17:48 -0500

    📖 Adding docs (experimenting with github pages)

---
+++ docs/precache-manifest.b56ad613c5f93cca6d6d36bfca2ad1ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2dae9195bf1e225a200",
+    "url": "/static/js/main.d2dae919.chunk.js"
+  },
+  {
+    "revision": "37e453d67a8e0dfa7dfe",
+    "url": "/static/js/1.37e453d6.chunk.js"
+  },
+  {
+    "revision": "d2dae9195bf1e225a200",
+    "url": "/static/css/main.15331104.chunk.css"
+  },
+  {
+    "revision": "37e453d67a8e0dfa7dfe",
+    "url": "/static/css/1.019e697e.chunk.css"
+  },
+  {
+    "revision": "3ecc2e4a27692920bbc50529ae24c6bf",
+    "url": "/index.html"
+  }
+];
