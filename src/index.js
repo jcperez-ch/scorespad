b856c0bbbfa0f169b608b6f174a6a5ebe6915c88
JC Perez Chavez <jc.perezchavez@gmail.com>
2019-01-02 13:59:31 -0500

    PWA Trying 🕸

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'reset-css/reset.css';
 
 import Root from './components/Root';
-import 'reset-css/reset.css';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 render(<Root />, rootElement);
+registerServiceWorker();
