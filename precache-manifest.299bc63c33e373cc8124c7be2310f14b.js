1b58108b83eac439ee335e23f23d40c64beb5654
JC Perez Chavez <jc.perezchavez@gmail.com>
2019-09-28 00:21:02 -0400

    Updates

---
+++ precache-manifest.299bc63c33e373cc8124c7be2310f14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e32d5ba5c35633f56385b2852557f9",
+    "url": "/scorespad/index.html"
+  },
+  {
+    "revision": "c77027b0b35e1f895db1",
+    "url": "/scorespad/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "9798d34f1e67a5baedc8",
+    "url": "/scorespad/static/css/main.cd74bc15.chunk.css"
+  },
+  {
+    "revision": "c77027b0b35e1f895db1",
+    "url": "/scorespad/static/js/2.2aa91eae.chunk.js"
+  },
+  {
+    "revision": "9798d34f1e67a5baedc8",
+    "url": "/scorespad/static/js/main.1f48c351.chunk.js"
+  },
+  {
+    "revision": "2375efbe2bd9b442eda4",
+    "url": "/scorespad/static/js/runtime-main.8b526840.js"
+  }
+]);
