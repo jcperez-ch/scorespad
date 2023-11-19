77890cb3559827507ae224cdce90e04c71f7c242
JC Perez Chavez <jc.perezchavez@gmail.com>
2023-11-19 18:00:40 -0500

    Updates

---
+++ static/js/gs.d9d2bf68.chunk.js
@@ -0,0 +1,14 @@
+"use strict";(globalThis.webpackChunkscorespad=globalThis.webpackChunkscorespad||[]).push([[989],{913:(e,s,r)=>{r.d(s,{Z:()=>i});var t=r(2791),n=r(5931),a=r(184);function o(e,s){return(0,a.jsx)(n.Z,{ref:s,direction:"up",...e})}const i=(0,t.forwardRef)(o)},9624:(e,s,r)=>{r.d(s,{Z:()=>o});var t=r(225),n=r(1123);const a=(0,t.Z)(n.Z)`
+  padding: 1rem 0 0.5rem;
+`;a.defaultProps={variant:"body2",gutterBottom:!0};const o=a},4647:(e,s,r)=>{r.d(s,{Z:()=>f});var t=r(2791),n=r(3400),a=r(9875),o=r(1123),i=r(225),l=r(6728),c=r(184);const d=i.Z.h4`
+  border-bottom: 1px solid ${l.lB.border};
+`,m=i.Z.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+`,h=(0,i.Z)(o.Z)`
+  && {
+    margin-left: 0.75rem;
+  }
+`;function u(e,s){let{children:r,onClose:t,...o}=e;return(0,c.jsx)(d,{ref:s,...o,children:(0,c.jsxs)(m,{children:[(0,c.jsx)(n.Z,{color:"secondary",variant:"outlined",onClick:t,size:"large",children:(0,c.jsx)(a.Z,{children:"close"})}),(0,c.jsx)(h,{children:r})]})})}h.defaultProps={component:"h4",variant:"overline"};const f=(0,t.forwardRef)(u)},4370:(e,s,r)=>{r.r(s),r.d(s,{default:()=>Z});var t=r(2791),n=r(6237),a=r(7689),o=r(741),i=r(4518),l=r(5289),c=r(7123),d=r(9157),m=r(5193),h=r(913),u=r(4647),f=r(9624),x=r(2059),g=r(184);function Z(){const{gameKey:e}=(0,a.UO)(),[s]=(0,t.useContext)(x.Z),[r,Z]=(0,t.useState)(),[p,j]=(0,t.useState)(!0),[b]=(0,n.$)(),v=(0,a.s0)(),y=(0,a.TH)(),C=(0,m.Z)("(min-width: 481px)"),S=()=>{j(!1)},k=(0,t.useCallback)((async e=>{Z(await o.toDataURL(document.getElementById("qrcode"),e,{width:290}))}),[]);return(0,t.useEffect)((()=>{const r=s[e];r&&k(JSON.stringify({id:e,...r}))}),[e,s,k]),(0,g.jsxs)(l.Z,{fullScreen:!0,id:"game-share-dialog","aria-labelledby":"game-share-dialog-title",open:p,TransitionComponent:h.Z,onClose:S,TransitionProps:{onExited:()=>{var e,s;v(null!==(e=null===(s=y.state)||void 0===s?void 0:s.from)&&void 0!==e?e:"/")}},children:[(0,g.jsx)(u.Z,{id:"game-share-dialog-title",onClose:S,children:b("title.shareGame")}),(0,g.jsxs)(d.Z,{children:[(0,g.jsx)(f.Z,{children:b("messages.confirmShareGame")}),(0,t.useMemo)((()=>(0,g.jsx)("div",{style:C?{}:{textAlign:"center"},children:r&&(0,g.jsx)("img",{src:r,alt:"qr code"})})),[r,C]),(0,g.jsxs)(f.Z,{children:[b("messages.confirmShareGame_step1"),(0,g.jsx)("br",{}),b("messages.confirmShareGame_step2"),(0,g.jsx)("br",{}),b("messages.confirmShareGame_step3"),(0,g.jsx)("br",{}),b("messages.confirmShareGame_step4")]})]}),(0,g.jsx)(c.Z,{children:(0,g.jsx)(i.Z,{variant:"contained",color:"primary",autoFocus:!0,onClick:S,children:b("button.ok")})})]})}}}]);
+//# sourceMappingURL=gs.d9d2bf68.chunk.js.map
