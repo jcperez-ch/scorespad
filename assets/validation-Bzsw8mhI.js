53aa37497ab238d37f0fa9d44c3bb25f36afcc66
JC Perez Chavez <jcperezch@meta.com>
2026-04-05 18:51:25 -0400

    Updates

---
+++ assets/validation-Bzsw8mhI.js
@@ -0,0 +1,1 @@
+import{a as u}from"./chunk-QFMPRPBF-DDhRdzzq.js";import{u as i}from"./useTranslation-A9g7j4rW.js";function f({name:r,onSubmit:e,errorMessage:o}){const[s]=i(),t=u.useRef(!1);return{error:t.current&&!r?s(o):void 0,onSubmit:()=>{r?e():t.current=!0}}}export{f as u};
